ff90094113fdac4f23c8240162e56ba36769aa05
yhlly <2890998645@qq.com>
2025-03-23 14:27:36 +0000

    Add files via upload

---
+++ setpaths.bat
@@ -0,0 +1,8 @@
+echo %cd% 
+
+set path=%cd%\apache-ant-1.10.5\bin;%path%
+
+set classpath=%cd%\lib\junit-4.12.jar;%cd%\lib\hamcrest-core-1.3.jar;.;%classpath%;
+
+set ANT_HOME=%cd%\apache-ant-1.10.5
+
